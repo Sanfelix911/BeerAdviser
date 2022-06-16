646c5173e0c704aa35aa2940f6bd896df2525bba
sanfelix <sanfelix@yandex.ru>
2022-06-16 21:33:13 +0900

    Организован отклик на нажатие кнопки выбора пива после выбора сорта из выпадающего списка

--- app/src/main/java/ru/sanfelix/beeradviser/FindBeerActivity.java
+++ app/src/main/java/ru/sanfelix/beeradviser/FindBeerActivity.java
@@ -3,6 +3,9 @@ package ru.sanfelix.beeradviser;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class FindBeerActivity extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class FindBeerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_beer);
+    }
+
+    public void onClickFindBeer(View view) {
+        TextView brands = (TextView) findViewById(R.id.brands);
+        Spinner color = (Spinner) findViewById(R.id.color);
+        String beerType = String.valueOf(color.getSelectedItem());
+        brands.setText(beerType);
     }
 }
